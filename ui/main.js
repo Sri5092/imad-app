80cf3dee273792ee20ba6537f7c80c301a426b91
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-22 20:38:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@ submit.onclick = function () {
        }
    };    
     
-    request.open('GET','http://sriekanth91.imad.hasura-app.io/counter',true);
+    request.open('GET','http://sriekanth91.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
     
