c3d1a26dc27ab61459f5296b1567d189e76da6f2
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-22 20:24:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/ui/main.js', function (req, res) {
 var names = [];
 app.get('/submit-name/:name',function(req,res){
    var name = req.params.name;
-   name.push(name);
+   names.push(name);
    res.send(JSON.stringify(names));
 });
 
