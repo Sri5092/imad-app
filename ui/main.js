921e4de732b07ab4572998ad25b1a805efba15ff
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-22 20:12:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ var nameinput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_button');
 submit.onclick = function(){
-    
+    alert('Hi');
     var names = ['name1','name2','name3'];
     var list = '';
     for(var i=0;i<names.length;i++){
