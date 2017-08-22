620468eb7f08f0e9c363a8d5f46d95e6f7188180
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-22 20:37:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,11 +23,23 @@ var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submit = document.getElementById('submit_button');
 submit.onclick = function () {
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var listitem = document.getElementById('list_name');
-    listitem.innerHTML = list;
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE){
+           if (request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                list += '<li>' + names[i] + '</li>';
+                }
+                var listitem = document.getElementById('list_name');
+                listitem.innerHTML = list;
+            }
+       }
+   };    
+    
+    request.open('GET','http://sriekanth91.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
 };
