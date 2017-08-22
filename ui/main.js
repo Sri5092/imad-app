8d9364e3f0c52f8b7864d9daaedd0bb86974e7ef
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-22 18:19:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main');
 element.innerHTML = 'New Value';
+
+var img = document.getElementById('img1');
+img.onclick = function(){
+    img.Style.marginLeft = '200px';
+}
