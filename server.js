c24d996e8ba97a21c32c19d68bf890a4cb32d406
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-23 19:13:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,9 +104,21 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/:articleName', function (req, res) {
-var articleName = req.params.articleName;
-  res.send(CreateTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+
+pool.query("SELECT * FROM article WHERE title = " + req.params.articleName,function(err,result){
+   if(err){
+       res.status(500).send(err.toString());
+   } else{
+       if(result.rows.length === 0){
+           res.status(404).send('Article Not Founc');
+       }else{
+           var articleData = result.rows[0];
+           res.send(CreateTemplate(articles[articleName]));
+       }
+   }
+});
+  
 });
 
 app.get('/article-two', function (req, res) {
