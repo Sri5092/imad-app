e6edf013ed92ff5340e51467598a05cd8c076591
Sri5092 <31200942+Sri5092@users.noreply.github.com>
2017-08-30 15:30:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
+var crypto = require('crypto');
 app.use(morgan('combined'));
 
 
@@ -95,6 +96,18 @@ app.get('/test-db',function(req,res){
        }
     });
 });
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+
+app.get('/hash/:input',function(req,res){
+   var harshedString = hash(req.params.input,'this-is-a-random-string');
+   res.send(harshedString);
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
